5b4baa067e739980baf0a78266ebac621ce7995f
tanneranorton23@gmail.com <tannort5@internet-measurement.cs.byu.edu>
2019-04-10 15:39:09 -0600

    enums and test01

--- validation/States.py
+++ validation/States.py
@@ -6,3 +6,20 @@ class States(enum.Enum):
     START = 0
     BASE = 1
     SUCCESS = 2
+
+    # DNS Record Types
+    A = "A"
+    A4 = "AAAA"
+    CNAME = "CNAME"
+    DNAME = "DNAME"
+    HIP = "HIP"
+    KX = "KX"
+    LOC = "LOC"
+    MX = "MX"
+    NAPTR = "NAPTR"
+    NS = "NS"
+    PTR = "PTR"
+    SMIMEA = "SMIMEA"
+    SOA = "SOA"
+    SRV = "SRV"
+    URI = "URI"
