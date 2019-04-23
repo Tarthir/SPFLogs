faa37d9d47135c176eafc6d9c8168386707bb01d
Tarthir <tylerbrady34@gmail.com>
2019-04-23 08:16:36 -0600

    quick change

--- validation/state_objs/SuperState.py
+++ validation/state_objs/SuperState.py
@@ -14,13 +14,13 @@ class SuperState(ABC):
         if self.get_result_method is not None:
             file_name = "validation_results/{}_results.txt".format(self.ending_log.test_name)
             with open(file_name, "a+") as f:
-                try:
-                    res = self.get_result_method(self.ending_log, log_list)
-                    if res is not None:
-                        f.write(res)
-                        f.flush()
-                except TypeError as err: # TODO get rid of this
-                    sys.stderr.write("Error:{} - func:{} - tName:{}\n".format(str(err), self.get_result_method, self.ending_log.test_name))
+               # try:
+                res = self.get_result_method(self.ending_log, log_list)
+                if res is not None:
+                    f.write(res)
+                    f.flush()
+                #except TypeError as err: # TODO get rid of this
+                #    sys.stderr.write("Error:{} - func:{} - tName:{}\n".format(str(err), self.get_result_method, self.ending_log.test_name))
         else:
             sys.stderr.write("SuperState: No get Result method given, please give state objects get_result method(s)\n")
 
