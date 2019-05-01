1ab68051b23581f7a72c610c054f394ee95bb8de
Tarthir <tylerbrady34@gmail.com>
2019-05-01 09:13:33 -0600

    Test 14: Only fails for b.t14...A or c.t14...A, best guess = OK

--- validation/Test14.py
+++ validation/Test14.py
@@ -1,7 +1,8 @@
 from validation.TestBase import TestBase
-from validation.state_objs.SuccessState import SuccessState
-from validation.state_objs.StartState import StartState
 from validation.state_objs.FailureState import FailureState
+from validation.state_objs.StartState import StartState
+from validation.state_objs.StateUtils import check_a
+from validation.state_objs.SuccessState import SuccessState
 
 
 class Test14(TestBase):
@@ -15,7 +16,8 @@ class Test14(TestBase):
     def test_def(self, log):
         if isinstance(self.state, StartState) and log.rec_queried == "TXT":
             self.state = SuccessState(log, self.get_test_result)
-        elif isinstance(self.state, SuccessState):
+        elif isinstance(self.state, SuccessState) and \
+                ((log.level == "b" or log.level == "c") and check_a(log.rec_queried)):
             self.state = FailureState(log, self.get_test_result)
 
     def get_test_result(self, log, log_list):
