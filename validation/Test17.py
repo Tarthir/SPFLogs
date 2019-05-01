e15c84830a12f40bcbdffc9a193fcdb907a21d9e
Tarthir <tylerbrady34@gmail.com>
2019-05-01 09:37:59 -0600

    Test 14: Only fails for b.t14...A or c.t14...A, best guess = OK Test 15: only fails for b.t15...A, best guess = OK Test 16: we need to know if we queried on ipv4 or ipv6 Test 17: count how many b...A queries there were

--- validation/Test17.py
+++ validation/Test17.py
@@ -1,24 +1,32 @@
-from validation.state_objs.FailureState import FailureState
-from validation.state_objs.SuccessState import SuccessState
-from validation.state_objs.StartState import StartState
 from validation.TestBase import TestBase
+from validation.state_objs.FailureState import FailureState
+from validation.state_objs.StartState import StartState
 from validation.state_objs.StateUtils import check_a
+from validation.state_objs.SuccessState import SuccessState
 
 
 class Test17(TestBase):
     def __init__(self):
         TestBase.__init__(self, self.test_def)
+        self.num_of_b_queries = 0
 
     def do_testing(self, log_list):
         # call to super class
+        self.num_of_b_queries = 0
         return TestBase.check_testing(self, log_list)
 
     def test_def(self, log):
         if isinstance(self.state, StartState) and log.level == "b" and check_a(log.rec_queried):
             self.state = SuccessState(log, self.get_test_result)
+            self.num_of_b_queries = 1
         elif isinstance(self.state, SuccessState) and log.level == "b" and check_a(log.rec_queried):
             self.state = FailureState(log, self.get_test_result)
+            self.num_of_b_queries += 1
         elif isinstance(self.state, FailureState):
+            # if there are more b queries
+            # TODO may need to change to only get b queries before l1-l5 queries?
+            if log.level == "b" and check_a(log.rec_queried):
+                self.num_of_b_queries += 1
             self.state = FailureState(log, self.get_test_result)
 
     def get_test_result(self, log, log_list):
@@ -27,4 +35,4 @@ class Test17(TestBase):
             cache = "Yes"
         else:
             cache = "No"
-        return "{} Caching:{}".format(TestBase.get_test_result(self, log, log_list), cache)
+        return "{} Caching:{} #of b{}".format(TestBase.get_test_result(self, log, log_list), cache, self.num_of_b_queries)
